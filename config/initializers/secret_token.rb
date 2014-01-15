a5dff3e998a70daf2de50f6344a946d750c63efe
Filly <hi@fillycampbell.com>
2014-01-15 23:51:47 +0000

    Inialising RoR project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Alex::Application.config.secret_key_base = 'b5acb6aeab7b0de9075a09b9bca4e0566876ea55b587fe68ab2ee94d67e7e1555441dc2d6c9376f2e9811c18697d329e24b17b7f806575ec8862fa229009a8f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Alex::Application.config.session_store :cookie_store, key: '_Alex_session'
